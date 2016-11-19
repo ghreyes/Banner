59c7e8228f0be0a30652c56cfa68f073ce5c6aee
Gabriel Reyes <ghreyes@ucsc.edu>
2016-11-18 16:39:59 -0800

    Added Chart.js and starter chart

--- models/tables.py
+++ models/tables.py
@@ -20,21 +20,21 @@ def get_email():
 
 #the code for user may better fit in db.py under the auth_user.extra_fields function
 db.define_table('person',
-		#Field('user_name', get_name()),
-        Field('user_email', default=auth.user.email if auth.user_id else None),
-        Field('gender', requires=IS_IN_SET(['Male', 'Female', 'Unspecified'], error_message='Please choose a category')),
-        #Field('job'), is our budget planner for students only?
-	Field('age', 'integer'),
-	Field('current_balance', 'double'),
-        Field('goal', 'double'),
-        Field('income', 'double'),
-        Field('rent', 'double'),
-	Field('food', 'double'),
-	Field('utilities', 'double'),
-	Field('transportation', 'double'),
-	Field('other', 'double'),
-        Field('total', 'double')
-        )
+		        #Field('user_name', get_name()),
+                Field('user_email', default=auth.user.email if auth.user_id else None),
+                Field('gender', requires=IS_IN_SET(['Male', 'Female', 'Unspecified'], error_message='Please choose a category')),
+                #Field('job'), is our budget planner for students only?
+                Field('age', 'integer'),
+                Field('current_balance', 'double'),
+                Field('goal', 'double'),
+                Field('income', 'double'),
+                Field('rent', 'double'),
+                Field('food', 'double'),
+                Field('utilities', 'double'),
+                Field('transportation', 'double'),
+                Field('other', 'double'),
+                Field('total', 'double')
+)
 
 # after defining tables, uncomment below to enable auditing
 auth.enable_record_versioning(db)
